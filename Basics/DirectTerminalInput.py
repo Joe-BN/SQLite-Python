8938bef19ae0872bb89f0f044d617247eb803965
JBN <joe.ngera@gmail.com>
2024-04-15 07:54:55 +0300

    Updating the DirectTerminalInput.py file by giving the standard way of input without using tuples.

--- Basics/DirectTerminalInput.py
+++ Basics/DirectTerminalInput.py
@@ -34,3 +34,23 @@ conn.commit()
 
 c.close()
 conn.close()
+
+#The input method I've used above may seem pointless but is usefull as a way to put in multiple prediterminrd value: using the tuple
+
+
+#A better way to go round this is to use the following:
+
+#while number < x:
+#    name = input(f"Input name for entry {number}: ")
+#    age = input(f"input age for {name}: ")
+#    height= input("Suggest a height for new User: ")
+
+#    c.execute('''
+#        INSERT INTO students1 (number, name, age, height) VALUES (?, ?, ?, ?)
+#        ''',
+#        (number, name, age, height))
+#    conn.commit()
+#    number += 1
+
+
+#An issue to note with this code is that every time it runs, counter starts from Zero
