c5982912738a6d5f7b264cf99b943b30f70ef89c
JBN <joe.ngera@gmail.com>
2024-04-15 18:32:01 +0300

    Adding more comments on options in BasicQuery.py

--- Basics/BasicQuery.py
+++ Basics/BasicQuery.py
@@ -9,6 +9,10 @@ c = conn.cursor()
 
 # Displaying data
 c.execute("SELECT * FROM students1 WHERE age=19") 
+# Also instead of selecting all, we can put the fields that we want to display(separating their name using commas) 
+# This way you can also preset an order for the way that theu will be presented
+# Though when doing this remember to remove the [x] next to row so thhat all the data you want to show, shows (unless that's not what you want :) )
+
 #The = sumbol can be replaces by logic operators eg: > incluting and 'and' for specified ranges
 total = 0
 for row in c.fetchall():
